d7eafab69b5ccf83b5a92388e80fc16f89a33c7b
MattFerguson1 <mferguson@lat-mferguson4.cb.careerbuilder.com careerbuilder.com>
2015-04-17 09:31:00 -0500

    Adding Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   get 'jobs/search', to: 'jobs#search', as: :jobs_search
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
